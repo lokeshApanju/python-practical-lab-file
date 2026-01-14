c0542fc8ef4d66b8d586abfb8e170de5d8a1f9f7
lokeshApanju <panjusahu7890@gmail.com>
2026-01-14 20:37:19 +0530

    Add files via upload

---
+++ Python/Practical Record/Q14_Create a package/main.py
@@ -0,0 +1,8 @@
+from shapes.circle import area as circle_area, perimeter as circle_perimeter
+from shapes.rectangle import area as rect_area, perimeter as rect_perimeter
+
+print("Circle Area:", circle_area(5))
+print("Circle Perimeter:", circle_perimeter(5))
+
+print("Rectangle Area:", rect_area(4, 6))
+print("Rectangle Perimeter:", rect_perimeter(4, 6))
---
+++ Python/Practical Record/Q14_Create a package/shapes/__init__.py
@@ -0,0 +1,2 @@
+from shapes import circle_area, rectangle_area
+from shapes import circle_perimeter, rectangle_perimeter
